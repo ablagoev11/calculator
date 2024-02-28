f103fc98fe894399515cf1ce73bb0b8bf86efe91
ablagoev11 <a.blagoev52@gmail.com>
2024-02-28 18:00:16 +0200

    Add floating points and fix "stringing" bugs

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
+const display = document.querySelector("h1");
+const actionDisplay = document.querySelector("h3");
+const displayContainer = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
+
 let arg1 = null,
   arg2 = null;
 let operator = null;
-let currentFloat = false;
 let isConcatable = false;
 let isEqual = false;
-const display = document.querySelector("h1");
-const actionDisplay = document.querySelector("h3");
-const buttons = document.querySelectorAll("button");
+const FONT_SIZE = "52px";
 const operatorEnum = {
   sum: "+",
   divide: "/",
@@ -28,12 +30,17 @@ buttons.forEach((element) => {
       case "8":
       case "9":
       case "0":
-        if (display.textContent === "0") display.textContent = "";
-        if (isConcatable) display.textContent += target.id;
-        else {
-          isConcatable = true;
-          if (isEqual) clear();
-          display.textContent = target.id;
+        if (display.textContent.length <= 15 || !isConcatable) {
+          if (display.textContent === "0") display.textContent = "";
+          if (isConcatable) {
+            display.textContent += target.id;
+            updateFont();
+          } else {
+            display.style.fontSize = FONT_SIZE;
+            isConcatable = true;
+            if (isEqual) clear();
+            display.textContent = target.id;
+          }
         }
         break;
       case "subtract":
@@ -41,16 +48,20 @@ buttons.forEach((element) => {
       case "multiply":
       case "divide":
         if (arg1 === null) {
-          arg1 = parseInt(display.textContent);
-        } else if (arg2 === null) {
-          arg2 = parseInt(display.textContent);
+          arg1 = parseFloat(display.textContent);
+        } else if (arg2 === null && isConcatable) {
+          arg2 = parseFloat(display.textContent);
           arg1 = action(operator, arg1, arg2);
           display.textContent = arg1;
+
           arg2 = null;
         } else {
           display.textContent = arg1;
+
+          arg2 = null;
           isEqual = false;
         }
+        updateFont();
         actionDisplay.textContent = `${arg1} ${target.textContent}`;
         isConcatable = false;
         operator = target.id;
@@ -60,13 +71,28 @@ buttons.forEach((element) => {
         break;
       case "equal":
         if (arg1 !== null && operator !== null) {
-          if (!arg2) arg2 = parseInt(display.textContent);
-          actionDisplay.textContent = `${arg1} ${operatorEnum[operator]} ${arg2} =`;
-          arg1 = action(operator, arg1, arg2);
-          display.textContent = arg1;
-          isConcatable = false;
-          isEqual = true;
+          if (isConcatable || isEqual) {
+            if (!arg2) arg2 = parseFloat(display.textContent);
+            actionDisplay.textContent = `${arg1} ${operatorEnum[operator]} ${arg2} =`;
+            arg1 = action(operator, arg1, arg2);
+            display.style.fontSize = FONT_SIZE;
+            display.textContent = arg1;
+            updateFont();
+            isConcatable = false;
+            isEqual = true;
+          }
         }
+        break;
+      case "float":
+        if (!display.textContent.includes(".")) {
+          display.textContent += ".";
+        }
+        if (isEqual) {
+          clear();
+          display.textContent += ".";
+        }
+        isConcatable = true;
+        updateFont();
         break;
     }
   });
@@ -113,5 +139,16 @@ function clear() {
   operator = null;
   isEqual = false;
   display.textContent = "0";
+  display.style.fontSize = FONT_SIZE;
   actionDisplay.textContent = "";
+}
+
+function updateFont() {
+  const style = window.getComputedStyle(display);
+  let font = style.getPropertyValue("font-size").slice(0, -2);
+  while (display.clientWidth + 20 > displayContainer.clientWidth) {
+    font -= 4;
+    console.log(font);
+    display.style.fontSize = `${font}px`;
+  }
 }
